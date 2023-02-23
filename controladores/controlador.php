3e812a003c8c2734e6952a8b127bbbc50e05aca5
DonJose00 <jcoronel.work@gmail.com>
2023-02-23 12:47:44 +0100

    prueba

--- controladores/controlador.php
+++ controladores/controlador.php
@@ -11,7 +11,6 @@ require_once 'C:\xampp\htdocs\tareaBlogServidor\modelos\modelo.php';
  */
 class controlador
 {
-
     // El el atributo $modelo es de la 'clase modelo' y será a través del que podremos
     // acceder a los datos y las operaciones de la base de datos desde el controlador
     private $modelo;
